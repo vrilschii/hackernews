8c844041d69b481e971e7f3a19e068cadd05a1df
Victor Rilschii <vrilschiI@gmail.com>
2020-04-13 21:48:59 -0400

    Complex JavaScript in JSX 2

--- src/App.js
+++ src/App.js
@@ -24,9 +24,20 @@ class App extends Component {
 	render() {
     return (
     	<div className="App">
-        {list.map(item => (<div>{item.title}</div>))}
+        { list.map(function(item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
       </div>
-    )
+    );
 	}
 }
 
