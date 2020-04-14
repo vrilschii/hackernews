891366f4e9d022fb73806ef3689d4db4d73ca116
Victor Rilschii <vrilschiI@gmail.com>
2020-04-13 21:40:39 -0400

    Complex JavaScript in JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook/github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 class App extends Component {
 	render() {
-    	const helloWorld = "Welcome to React";
-    	return (
-      		<div className="App">
-        		<h2>{helloWorld}</h2>
-      		</div>
-		  );
+    return (
+    	<div className="App">
+        {list.map(item => (<div>{item.title}</div>))}
+      </div>
+    )
 	}
 }
 
